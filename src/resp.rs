9a47a89fdbdb1603bdbf3b968896bf780884e5f5
Payton Bice <hellopayton@gmail.com>
2025-09-21 11:35:00 -0500

    Adds new methods and constants to `resp` crate - Adds a few constants for common byte sequences:   - Delimiter / terminator   - Ok responses   - Empty string responses - Implements `RespSerialize` for `RespElement` - Adds `RespElement::new_array()` and `RespElement::new_bulk_string()` as proxies for their constructors

--- src/resp.rs
+++ src/resp.rs
@@ -3,7 +3,11 @@ pub mod types;
 pub mod commands;
 
 use crate::resp::types::*;
-use crate::resp::parser::{RespElementConstructor, RespParseError};
+use crate::resp::parser::{RespElementConstructor, RespParseError, RespSerialize};
+
+pub const RESP_DELIMITER: &[u8] = b"\r\n";
+pub const RESP_OK: &[u8] = b"+OK\r\n";
+pub const RESP_EMPTY_STRING: &[u8; 5] = b"$-1\r\n";
 
 #[derive(Debug)]
 pub enum RespElement {
@@ -11,6 +15,27 @@ pub enum RespElement {
     SimpleString(RespSimpleString),
     BulkString(RespBulkString),
     Integer(RespInteger),
+}
+
+impl RespElement {
+    pub fn new_array(elements: Vec<RespElement>) -> RespElement {
+        RespElement::Array(RespArray::new(elements))
+    }
+
+    pub fn new_bulk_string(value: &[u8]) -> RespElement {
+        RespElement::BulkString(RespBulkString::new(value))
+    }
+}
+
+impl RespSerialize for RespElement {
+    fn to_bytes(&self) -> Vec<u8> {
+        match self {
+            RespElement::Array(a) => a.to_bytes(),
+            RespElement::BulkString(b) => b.to_bytes(),
+            RespElement::Integer(i) => i.to_bytes(),
+            RespElement::SimpleString(s) => s.to_bytes(),
+        }
+    }
 }
 
 impl RespElementConstructor for RespElement {
